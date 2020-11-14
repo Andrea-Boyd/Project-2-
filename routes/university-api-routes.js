47e2b5596db9cdb44f68ebb132bab7d8a47e2ea8
Andrea Boyd <andreanboyd@yahoo.com>
2020-11-14 13:53:54 -0600

    restored cityData.html

--- routes/university-api-routes.js
+++ routes/university-api-routes.js
@@ -1,1 +1,26 @@
-//might not need this route
+var db = require("../models");
+const university = require("../models/university");
+
+// Routes
+// =============================================================
+module.exports = function (app) {
+    app.get("/api/city/:univ", function (req, res) {
+        console.log(req)
+        db.City.findAll({
+            //include the universities tables
+            where:{
+                '$Univs.name$':req.params.univ
+            },
+            include: [{
+                model: db.Univ
+            },
+            {
+                model: db.Qol
+            }]
+        }).then(function (cityAndUniv) {
+            console.log(cityAndUniv);
+            res.json(cityAndUniv);
+        });
+    });
+
+};
