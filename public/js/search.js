815cf9f6846845cd8fef18e7d64fd796e166f347
Andrea Boyd <andreanboyd@yahoo.com>
2020-11-16 20:30:18 -0600

    handlebars functionality for city data display

--- public/js/search.js
+++ public/js/search.js
@@ -1,26 +1,28 @@
 
-let university = $("#search").val();
+//let university = $("#search").val();
 
 
 $("#submit").on("click", function (event) {
   event.preventDefault();
+  let university = $("#search").val();
   // const search = $("#search").val();
   // console.log(search);
   console.log(university)
-  getQOL("atlanta");
+  getQOL(university);
 
   //need to get corresponding id 
   //get api route for pulling our city page with our user generated data from our forum
 });
 
 
-function getQOL(city) {
+function getQOL(univ) {
   $.ajax({
     method: "GET",
-    url: "/api/"+city,
-    data: "Tells me about the city of Atlanta"
+    url: "/api/city/" + univ,
+    
   })
-    .then(function () {
-      window.location.href = "/forum";
+    .then(function (data) {
+      console.log(data);
+      window.location.href = "/cityData/" + data[0].id;
     });
 }
