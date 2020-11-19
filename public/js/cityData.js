c853af171230f3f2d09e6691db082cd43343acef
Tahliyah Mims <mimst97@gmail.com>
2020-11-18 18:06:31 -0600

    Fixed cityData handlerbars

--- public/js/cityData.js
+++ public/js/cityData.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
 
-  //This will correspond to the returned results for a city's search results should it be a sep. page
-  //taking in the University name, pulling the id and getting the city associated
-
   });
 
 
